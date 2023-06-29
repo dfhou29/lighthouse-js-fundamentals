57895002a3300f23150dcce685911af5a5baae02
Difan <dfhou29@gmail.com>
2023-06-29 01:48:29 -0700

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,19 @@
 const temperature = 20;
-
+const isRaining = true;
 if (temperature < 0) {
 	console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
 	console.log("Short sleeves won't cut it!");
 } else {
 	console.log("Short sleeves are fine.");
+}
+
+if (temperature < -40 || temperature > 40) {
+	console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!isRaining) {
+	console.log("Leave your umbrella at home!");
 }
 
 console.log("Now you're ready to go outside!");
